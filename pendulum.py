3a85c8c4e46cbf6148f7d1da617a0e7908c67cb0
Bowen Tian <bowen.tian@mail.mcgill.ca>
2017-05-21 21:05:15 -0400

    update (still opencv3)

--- pendulum.py
+++ pendulum.py
@@ -2,17 +2,21 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture('GOPR0173(1).avi')
-
+timeCount = 0
+trial = 1
+frame = 0
+x=0
+y=0
 while True:
 	ret, frame1 = cap.read()
-	
+	ret, frame2 = cap.read()
 	#grayImage1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 	
-	ret, frame2 = cap.read()
+	
 	#grayImage2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 	#diffImage = cv2.absdiff(grayImage1, grayImage2)
 	diffImage = cv2.absdiff(frame1, frame2)
-	retral, threshold = cv2.threshold(diffImage, 12, 255, cv2.THRESH_BINARY)
+	retral, threshold = cv2.threshold(diffImage, 20, 21, cv2.THRESH_BINARY)
 	grayImage = cv2.cvtColor(threshold, cv2.COLOR_BGR2GRAY)
 	
 	kernel = np.ones((15,15), np.float32) / 225
@@ -21,18 +25,31 @@ while True:
 	blur = cv2.GaussianBlur(grayImage, (15,15), 0)
 	retral2, final = cv2.threshold(blur, 12, 255, cv2.THRESH_BINARY)
 	
-	cv2.imshow('frame',final)
-	th, contours, hierarchy = cv2.findContours(final, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	cv2.imshow('Threshold of Blur',final)
+	th, contours, hierarchy = cv2.findContours(final, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	
-	if len(contours) > 0 & len(contours) < 10:
-		point = cv2.boundingRect(contours[-1])
-		x = (point[0] + point[1])/2
-		y = (point[2] + point[3])/2
-		if (x>300 & y>200) :
-			print x
-			print y
+	if len(contours) > 0 and len(contours) < 6 :
 		
-	
+		point = cv2.boundingRect(contours[-1])
+		
+		timeCount = 0
+		frame = frame + 1
+		x = point[0]
+		y = point[1]
+		print 'trial: '+ str(trial)+ ' frame: ' + str(frame) + ' x: ' + str(point[0]) +' y: ' + str(point[1])
+			
+			
+		
+	elif (timeCount > 20 and frame > 100):
+		timeCount = 0
+		frame = 0
+		trial = trial +1
+	else :
+		
+		timeCount = timeCount+1
+		frame = frame + 1
+		print 'trial: '+ str(trial)+ ' frame: ' + str(frame) + ' x: ' + str(x) +' y: ' + str(y)
+
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 cap.release()
